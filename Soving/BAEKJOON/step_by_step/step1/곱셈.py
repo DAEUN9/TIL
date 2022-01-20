783f2d98d6eeb5d15c08e778fd17b67ce666cfe2
DAEUN9 <gain3436@gmail.com>
2022-01-21 01:20:05 +0900

    solve

--- Soving/BAEKJOON/step_by_step/step1/곱셈.py
+++ Soving/BAEKJOON/step_by_step/step1/곱셈.py
@@ -2,9 +2,9 @@ A=int(input())
 B=input()
 result=0
 position=0
-for i in reversed(B)
-    curr=int(i)A
+for i in reversed(B):
+    curr=int(i)*A
     print(curr)
-    result+=curr10position
+    result+=curr*10**position
     position+=1
 print(result)
