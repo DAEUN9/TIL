e56589c767d8ab1c4e730386507e4009fd66d9db
DAEUN9 <gain3436@gmail.com>
2022-02-02 14:49:22 +0900

    0202

--- Soving/BAEKJOON/step_by_step/step9/소수.py
+++ Soving/BAEKJOON/step_by_step/step9/소수.py
@@ -1,19 +1,16 @@
 start = int(input())
 end = int(input())
-answer = 0
+
 total = []
-minimum = start
 for num in range(start, end+1):
-    for n in range(2, num):
+    for n in range(2, num+1):
+        if n == num:
+            total += [num]
         if num%n == 0:            
             break
-    else:
-        total += [num]
-        answer += num
-    if num == 1:
-        total -= [1]
+
 if total == []:
     print(-1)
 else:
-    print(answer)
+    print(sum(total))
     print(total[0])
---
+++ Soving/BAEKJOON/step_by_step/step9/소인수분해.py
@@ -0,0 +1,11 @@
+N = int(input())
+a = 2
+insu = []
+while N > 1 :
+    if N%a == 0:
+        insu.append(a)
+        N = N//a
+        a = 2
+    else:
+        a += 1
+print(insu)
