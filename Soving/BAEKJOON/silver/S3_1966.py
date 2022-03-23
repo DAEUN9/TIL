fcb9d484d76e67b3a8498cb3bf254a6dde4e5406
DAEUN9 <gain3436@gmail.com>
2022-03-24 00:04:11 +0900

    0324

--- Soving/BAEKJOON/silver/S3_1966.py
+++ Soving/BAEKJOON/silver/S3_1966.py
@@ -1,25 +1,45 @@
-# import sys
-#
-# sys.stdin = open('input.txt', 'r')
+# 프린터 큐
+# https://www.acmicpc.net/problem/1966
+
+import sys
+
+sys.stdin = open('input.txt', 'r')
 
 T = int(input())
 for t in range(T):
+    # 1번 풀이
+    # N, M = map(int, input().split())
+    # li = list(map(int, input().split()))
+    # cnt = 0
+    # while li:
+    #     if li[0] >= max(li):
+    #         cnt += 1
+    #         if M == 0:
+    #             answer = cnt
+    #             break
+    #         li.pop(0)
+    #         M -= 1
+    #     elif li[0] < max(li):
+    #         if M == 0:
+    #             M = len(li) - 1
+    #         else:
+    #             M -= 1
+    #         li.append(li.pop(0))
+    #
+    # print(cnt)
+
     N, M = map(int, input().split())
     li = list(map(int, input().split()))
     cnt = 0
     while li:
-        if li[0] >= max(li):
+        curr = li.pop(0)
+        if max(li+[0]) <= curr:
             cnt += 1
             if M == 0:
-                answer = cnt
                 break
-            li.pop(0)
-            M -= 1
-        elif li[0] < max(li):
-            if M == 0:
-                M = len(li) - 1
-            else:
-                M -= 1
-            li.append(li.pop(0))
-
+        else:
+            li.append(curr)
+        M -= 1
+        if M < 0:
+            M = len(li) - 1
     print(cnt)
