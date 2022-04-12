41f51334088a5f0b4d8417884707fd9335a52ddd
DAEUN9 <gain3436@gmail.com>
2022-04-13 00:19:03 +0900

    0413

--- Soving/BAEKJOON/silver/S1_1074.py
+++ Soving/BAEKJOON/silver/S1_1074.py
@@ -1,13 +1,32 @@
-import sys
-sys.stdin = open('input.txt','r')
+# Z
+# https://www.acmicpc.net/status?from_mine=1&problem_id=1074&user_id=asd5687
 
-N, r, c = map(int, input().split())
-arr=[[0]*N**2 for _ in range(N**2)]
-print(arr)
-cnt = 0
-for x, y in [(0, 0), (0, 1), (-1, 0), (-1, 1)]:
-    nr = r + x
-    nc = c + y
-    arr[nr][nc] = cnt
-def Z(n):
-    n = n//2
+import sys
+
+sys.stdin = open('input.txt', 'r')
+input=sys.stdin.readline
+
+n,r,c=map(int,input().split())
+ans=0
+
+def solve(x,y,n):
+    global ans, flag
+    if flag:
+        return
+    if x==r and y==c:
+        flag= True
+        return
+    if n==1:
+        ans+=1
+        return
+    if not (x<=r<x+n and y<=c<y+n):
+        ans+=n*n
+        return
+    temp=n//2
+    solve(x,y,temp)
+    solve(x,y+temp,temp)
+    solve(x+temp,y,temp)
+    solve(x+temp,y+temp,temp)
+flag = False
+solve(0,0,2**n)
+print(ans)
--- Soving/BAEKJOON/silver/check_S1_1074.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-sys.stdin = open('input.txt', 'r')
-# 2**N * 2**N 배열, 목표 행, 목표 열
-N, r, c = map(int, input().split())
