384d91c185e212f014c048b8e9a27833eaa63c7a
DAEUN9 <gain3436@gmail.com>
2022-05-09 00:54:36 +0900

    0509

--- Soving/BAEKJOON/gold/G5_14567.py
+++ Soving/BAEKJOON/gold/G5_14567.py
@@ -1,12 +1,18 @@
+# 선수과목
+# https://www.acmicpc.net/problem/14567
+
 import sys
 from collections import deque
-sys.stdin = open('input.txt','r')
+
+# sys.stdin = open('input.txt','r')
+
 input = sys.stdin.readline
 v, e = map(int, input().split())
 
 indegree = [0]*(v+1)
 
 graph = [[] for _ in range(v+1)]
+semester = [0] * (v + 1)
 
 for _ in range(e):
     a, b = map(int, input().split())
@@ -14,20 +20,20 @@ for _ in range(e):
     indegree[b] += 1
 
 def topology_sort():
-    result = []
     q = deque()
+
     for i in range(1, v+1):
         if indegree[i] == 0:
             q.append(i)
-
+            semester[i] = 1
     while q:
         now = q.popleft()
-        result.append(now)
         for i in graph[now]:
             indegree[i] -= 1
             if indegree[i] == 0:
                 q.append(i)
-    for i in result:
-        print(i, end=' ')
+                semester[i] = semester[now] + 1
+
 
 topology_sort()
+print(*semester[1:])
