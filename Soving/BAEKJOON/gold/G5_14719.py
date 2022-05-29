b81975442417141f0cdd1d8c59d1613f0fb0b33f
DAEUN9 <gain3436@gmail.com>
2022-05-29 22:02:36 +0900

    0529

--- Soving/BAEKJOON/gold/G5_14719.py
+++ Soving/BAEKJOON/gold/G5_14719.py
@@ -5,16 +5,12 @@ sys.stdin = open('input.txt', 'r')
 H, W = map(int, input().split())
 
 li = list(map(int, input().split()))
-edge = 0
-max_num = 0
 total = 0
-for i in range(1, W):
-    if li[i] >= li[edge]:
-        for j in range(edge+1, i):
-            total += li[edge] - li[j]
-        edge = i
-        max_num = 0
-    else:
-        if li[i] > max_num:
-            max_num = i
+
+for i in range(1, W-1):
+    left = max(li[:i])
+    right = max(li[i+1:])
+    m = min(left, right)
+    if m> li[i]:
+        total += m-li[i]
 print(total)
