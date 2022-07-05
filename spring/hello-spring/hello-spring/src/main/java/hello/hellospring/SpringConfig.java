605b836f57fffd06bcc0a5636a846c3de1b20ac1
DAEUN9 <gain3436@gmail.com>
2022-07-05 23:00:51 +0900

    0705

--- spring/hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ spring/hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,6 +1,5 @@
 package hello.hellospring;
 
-import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,10 +21,10 @@ public class SpringConfig {
         return new MemberService(memberRepository);
     }
 
-    @Bean
-    public TimeTraceAop timeTraceAop() {
-        return new TimeTraceAop();
-    }
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
 //    @Bean
 //    public MemberRepository memberRepository() {
 //        return new JdbcMemberRepository(dataSource);
