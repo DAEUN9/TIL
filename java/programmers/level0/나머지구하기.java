aa01e6fa4b01c506052dc7c8ae191d6a95df2d5f
DAEUN9 <gain3436@gmail.com>
2022-10-12 23:15:34 +0900

    1012

---
+++ java/programmers/level0/나머지구하기.java
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120810?language=java
+
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1%num2;
+        return answer;
+    }
+}
