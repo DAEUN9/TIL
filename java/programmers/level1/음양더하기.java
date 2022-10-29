44ffda2d66b511dc6a12003fc1d7af1b9276bbea
daeun <gain3436@gmail.com>
2022-10-29 23:40:55 +0900

    1029

---
+++ java/programmers/level1/음양더하기.java
@@ -0,0 +1,15 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/76501
+
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i=0; i<signs.length; i++) {
+            if (signs[i]==true) {
+                answer += absolutes[i];
+            } else {
+                answer -= absolutes[i];
+            }
+        }
+        return answer;
+    }
+}
