bee9f90b3610ff3e2fbc164af3284fff03eaacaf
daeun <gain3436@gmail.com>
2022-11-24 23:58:14 +0900

    1124

---
+++ java/programmers/level0/각도기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int angle) {
+        if (angle<90) {
+        return 1;
+    }
+    if (angle == 90) {
+        return 2;
+    }
+    if (angle < 180) {
+        return 3;
+    }
+    return 4;
+    }
+}
