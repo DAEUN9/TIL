23ac5cbc6deafc0a917b9e4c6c2703b7eb521edc
DAEUN9 <gain3436@gmail.com>
2022-12-22 09:48:02 +0900

    1222

---
+++ java/backjoon/step/step1/B5_10998.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class B5_10998 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        scanner.close();
+        System.out.println(a*b);
+    }
+}
