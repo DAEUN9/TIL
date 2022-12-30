61a7a9b6ec34bf0d5fb71275c716910f9128b66e
DAEUN9 <gain3436@gmail.com>
2022-12-30 16:41:50 +0900

    1230

---
+++ java/backjoon/step/step1/B5_10171.java
@@ -0,0 +1,9 @@
+public class B5_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|"
+        );
+    }
+}
