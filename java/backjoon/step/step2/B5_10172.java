a7e8c5f4119231b3796b549d83b961a32330f00f
DAEUN9 <gain3436@gmail.com>
2023-01-03 15:31:19 +0900

    010 3

---
+++ java/backjoon/step/step2/B5_10172.java
@@ -0,0 +1,9 @@
+public class B5_10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+    }
+}
---
+++ java/backjoon/step/step2/B5_25083.java
@@ -0,0 +1,10 @@
+public class B5_25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
