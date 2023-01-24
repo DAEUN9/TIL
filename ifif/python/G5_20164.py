68a0aebffb78e64a52f997ecc75b01ee95f6bee4
DAEUN9 <gain3436@gmail.com>
2023-01-24 22:02:47 +0900

    TIL : 스터디 백준20164 python 풀이

--- ifif/python/G5_20164.py
+++ ifif/python/G5_20164.py
@@ -2,30 +2,34 @@ import sys
 
 sys.stdin = open("input.txt","r")
 N = input()
-b = []
+answer = [1e9, 0]
 
 
 def solution(N, cnt):
     if len(N)==1:
-        return cnt + checkOdd(int(N))*2
+        answer[0] = min(cnt, answer[0])
+        answer[1] = max(cnt, answer[1])
+        return
     if len(N)==2:
-        return checkOdd(int(N[0]))*2 + checkOdd(int(N[1]))*2 + cnt
+        N = str(int(N[0])+int(N[1]))
+        return solution(N, cnt+checkOdd(N))
     for i in range(1, len(N)-1):
         for j in range(i+1, len(N)):
                 a = int(N[:i])
                 b = int(N[i:j])
                 c = int(N[j:])
                 num = checkOdd(a+b+c)
-                print(solution(str(a+b+c), cnt+num))
-                return
+                solution(str(a+b+c), cnt+num)
 
 def checkOdd(num):
     cnt = 0
-    while num>1:
-        n = num%2
-        num %= 2
-        if n==0:
-           cnt += 1
+    for n in str(num):
+        if n in "13579":
+            cnt+=1
     return cnt
 
 solution(N, 0)
+origin = checkOdd(N)
+answer[0] += origin
+answer[1] += origin
+print(*answer)
