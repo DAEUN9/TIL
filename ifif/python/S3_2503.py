ac54da7081334135dcdc34ce1270c090248868f3
DAEUN9 <gain3436@gmail.com>
2023-02-01 05:08:04 +0900

    TIL : 스터디 백준 2503 python 풀이

--- ifif/python/S3_2503.py
+++ ifif/python/S3_2503.py
@@ -5,20 +5,38 @@ import sys
 
 sys.stdin = open("input.txt", "r")
 T = int(input())
-answer = set(str(i) for i in range(1, 10))
-stList = [set(),set(),set()]
-ballList = set()
-for _ in range(T):
-    N, st, ball = map(int, input().split())
-    numList = list(str(N))
-    if st ==0 and ball ==0:
-        for num in numList:
-            answer.remove(num)
-        continue
-    if st:
-        for i in range(3):
-            stList[i].add(numList[i])
-    if ball:
-        ballList.update(set(numList).intersection(answer))
-print(ballList)
-print(stList)
+inputList = [list(map(int, input().split())) for _ in range(T)]
+answer = 0
+
+def count(num, answer):
+    ball = 0
+    strike = 0
+    for i in range(3):
+        if num[i] == answer[i]:
+            strike+=1
+        elif num[i] in answer:
+            ball += 1
+    return strike, ball
+
+def check(answer):
+    for inp in inputList:
+        curr = str(inp[0])
+        strike, ball = count(curr, answer)
+        if strike != inp[1]:
+            break
+        if ball != inp[2]:
+            break
+    else:
+        return 1
+    return 0
+
+
+for a in range(1, 10):
+    for b in range(1, 10):
+        if a==b:
+            continue
+        for c in range(1, 10):
+            if b==c or a==c:
+                continue
+            answer += check(str(a)+str(b)+str(c))
+print(answer)
