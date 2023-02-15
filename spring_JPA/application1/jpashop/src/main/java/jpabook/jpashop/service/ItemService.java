d3819b22f3b76c5216f6ede779580cd1f96cb2f5
DAEUN9 <gain3436@gmail.com>
2023-02-15 11:28:17 +0900

    TIL : JPA 활용1 section7 상품기능 정리

--- spring_JPA/application1/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ spring_JPA/application1/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 @Service
@@ -14,9 +15,36 @@ import java.util.List;
 public class ItemService {
 
     private final ItemRepository itemRepository;
+
+    private final EntityManager em;
+
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional
+    // DirtyCheck
+    void update(Item itemParam) { //itemParam: 파리미터로 넘어온 준영속 상태의 엔티티
+        Item findItem = em.find(Item.class, itemParam.getId()); //같은 엔티티를 조회한다.
+                findItem.setPrice(itemParam.getPrice()); //데이터를 수정한다.
+    }
+
+    @Transactional
+    void updateMerge(Item itemParam) { //itemParam: 파리미터로 넘어온 준영속 상태의 엔티티
+        Item mergeItem = em.merge(itemParam);
+    }
+
+    /**
+     * 영속성 컨텍스트가 자동 변경
+     */
+    @Transactional
+    public void updateItem(Long id, String name, int price, int stockQuantity)
+    {
+        Item item = itemRepository.findOne(id);
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
--- spring_JPA/application1/jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ spring_JPA/application1/jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -5,9 +5,12 @@ import jpabook.jpashop.domain.Item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -57,9 +60,9 @@ public class OrderService {
 
 
     /** 주문 검색 */
-/*
- public List<Order> findOrders(OrderSearch orderSearch) {
- return orderRepository.findAll(orderSearch);
- }
-*/
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAll(orderSearch);
+//        return orderRepository.findAllString(orderSearch);
+    }
+
 }
