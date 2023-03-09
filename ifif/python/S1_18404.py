932d2fdd26a4e7dd5173b4de680855443d3ff21c
DAEUN9 <gain3436@gmail.com>
2023-03-10 02:13:30 +0900

    TIL : 백준 18404 python 풀이

--- ifif/python/S1_18404.py
+++ ifif/python/S1_18404.py
@@ -1,1 +1,23 @@
 import sys
+from collections import deque
+sys.stdin = open("input.txt", "r")
+N, M = map(int, input().split())
+X, Y = map(int, input().split())
+another = [list(map(int, input().split())) for _ in range(M)]
+direction = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1)]
+dq = deque()
+dq.append([X, Y, 0])
+visited = [[0]*(N+1) for _ in range(N+1)]
+visited[X][Y] = 1
+while dq:
+    x, y, cnt = dq.popleft()
+
+    for a, b in direction:
+        nx = a +x
+        ny = b +y
+        if 0<nx<=N and 0<ny<=N and not visited[nx][ny]:
+            visited[nx][ny] = cnt+1
+            dq.append([nx, ny, cnt+1])
+
+for c, d in another:
+    print(visited[c][d], end=" ")
