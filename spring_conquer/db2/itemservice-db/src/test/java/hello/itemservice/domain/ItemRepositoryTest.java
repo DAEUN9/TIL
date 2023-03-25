8bc2e6488c8be419ce9f43edc00f00f297b59cc7
DAEUN9 <asd5687@naver.com>
2023-03-26 00:55:52 +0900

    TIL : 스프링 db2 실습

--- spring_conquer/db2/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ spring_conquer/db2/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -8,16 +8,26 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 @SpringBootTest
 class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+//    @Autowired
+//    PlatformTransactionManager transactionManager;
+//    TransactionStatus status;
+//    @BeforeEach
+//    void beforeEach() {
+//        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+//    }
 
     @AfterEach
     void afterEach() {
@@ -25,6 +35,8 @@ class ItemRepositoryTest {
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
+        //트랜잭션 롤백
+//        transactionManager.rollback(status);
     }
 
     @Test
