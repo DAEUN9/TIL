90ed56bccbf03da443068f783c49b13536ef71ba
DAEUN9 <gain3436@gmail.com>
2023-05-04 01:04:37 +0900

    TIL : 프로그래머스 python 풀이

--- Soving/programmers/level3/미로탈출명령어.py
+++ Soving/programmers/level3/미로탈출명령어.py
@@ -1,26 +1,37 @@
+import sys
+
+sys.setrecursionlimit(5000)
+
+
 def solution(n, m, x, y, r, c, k):
     global answer
     # 시작점 x, y
     # 도착점 r, c
-    answer = 'z'*k
+    answer = 'impossible'
 
-    dfs(x-1, y-1, r-1, c-1, n, m, '', k)
+    dfs(x - 1, y - 1, r - 1, c - 1, n, m, '', 0, k)
     return answer
 
-def dfs(a, b, r, c, n, m, s, k):
+
+def dfs(a, b, r, c, n, m, s, cnt, k):
     global answer
-    if len(s) == k:
-        if a==r and b==c and answer > s:
+    if answer != 'impossible':
+        return
+    if cnt == k:
+        if a == r and b == c:
             answer = s
         return
-    if b-1 >= 0:
-        dfs(a, b-1, r, c, n, m, s+'l', k)
-    if b+1 < n:
-        dfs(a, b+1, r, c, n, m, s+'r', k)
-    if a-1 >= 0:
-        dfs(a-1, b, r, c, n, m, s+'u', k)
-    if a+1 < m:
-        dfs(a+1, b, r, c, n, m, s+'d', k)
+    distance = abs(a - r) + abs(b - c)
+    if distance > k - cnt:
+        return
+    if abs(distance - (k - cnt)) % 2:
+        return
 
-answer = solution(3,	4,	2,	3,	3,	1,	5)
-print(answer)
+    if a + 1 < n:
+        dfs(a + 1, b, r, c, n, m, s + 'd', cnt + 1, k)
+    if b - 1 >= 0:
+        dfs(a, b - 1, r, c, n, m, s + 'l', cnt + 1, k)
+    if b + 1 < m:
+        dfs(a, b + 1, r, c, n, m, s + 'r', cnt + 1, k)
+    if a - 1 >= 0:
+        dfs(a - 1, b, r, c, n, m, s + 'u', cnt + 1, k)
