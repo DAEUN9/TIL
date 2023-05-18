c33840a6d5f05e79569bd88962e8743eb8872bef
DAEUN9 <gain3436@gmail.com>
2023-05-18 23:47:49 +0900

    TIL : 백준 python 풀이

--- ifif/python/S2_1411.py
+++ ifif/python/S2_1411.py
@@ -1,27 +1,24 @@
 import sys
 sys.stdin = open("input.txt", "r")
 N = int(input())
-words = list(input() for _ in range(N))
+words = list()
 answer = 0
-def check(a, b):
+for _ in range(N):
+    word = input()
     d = dict()
-    length = len(a)
-    for l in range(length):
-        if a[l] != b[l]:
-            if d.get(a[l], 0) == 0:
-                b = b.replace(b[l], a[l])
-                d[a[l]] = 1
-            else:
-                return False
+    idx = 1
+    temp = []
+    for w in word:
+        if d.get(w):
+            temp.append(d[w])
         else:
-            d[a[l]] = 1
-    if a == b:
-        return True
-    return False
-
+            d[w] = idx
+            temp.append(d[w])
+            idx += 1
+    words.append(temp)
 for i in range(N-1):
     for j in range(i+1, N):
-        if check(words[i], words[j]):
+        if words[i] == words[j]:
             answer += 1
 
 print(answer)
