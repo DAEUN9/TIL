28dbe32972ec1fcb9ee0a71143d7fbb99a6e4d08
DAEUN9 <gain3436@gmail.com>
2023-06-24 21:17:10 +0900

    TIL : 백준 python 풀이

--- ifif/python/S2_23082.py
+++ ifif/python/S2_23082.py
@@ -3,32 +3,39 @@ sys.stdin = open("input.txt", "r")
 N = int(input())
 num = [0, 1, -1]
 curr = 1
-answer = 0
-target = [0, 0]
+answer = ''
+target = 1
+if N == 0:
+    answer = 0
 if N < 0:
     minus = True
 else:
     minus = False
 N = abs(N)
 for i in range(1, N):
-    curr *= i
+    curr = i**3
     if curr >= N:
-        target = [i-1, i]
+        target = i
         break
-def make_three(n, string, idx):
-    global N, answer
-    if N == n:
-        answer = string
-        return
-    if target[1] <= idx:
-        return
-    for u in num:
-        if u == -1:
-            ss = 'T'
-        else:
-            ss = str(u)
-        make_three(n + u*3**idx, ss + string, idx+1)
-make_three(0, '', 0)
+while N:
+    sup = N%3
+    if sup == 2:
+        answer = 'T'+answer
+        N += 1
+    else:
+        answer = str(sup)+answer
+        N += -sup
+    N //= 3
+
 if minus:
-    answer = answer[::-1]
+    temp = ''
+    for a in answer:
+        if a == '1':
+            temp += 'T'
+        elif a == 'T':
+            temp += '1'
+        else:
+            temp += '0'
+    answer = temp
+
 print(answer)
