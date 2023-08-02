aae792fa3895851703da81a4978f10a2abbb304a
DAEUN9 <gain3436@gmail.com>
2023-08-02 23:34:20 +0900

    TIL : 백준 python 풀이

---
+++ ifif/python/S2_21396.py
@@ -0,0 +1,17 @@
+import sys
+from collections import Counter
+T = int(sys.stdin.readline())
+
+num = 0
+
+for i in range(T):
+	n, x = list(map(int, sys.stdin.readline().split()))
+    v = Counter(list(map(int, sys.stdin.readline().split())))
+    if x == 0:
+    	for c in v.keys():
+        	num += (v[c] * (v[c] - 1))
+    else:
+    	for c in v.keys():
+        	num += (v[c] * v[x^c])
+    print(num // 2)
+    num = 0
