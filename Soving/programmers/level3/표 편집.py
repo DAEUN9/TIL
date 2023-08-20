cf3c427fff0b726794097abdd580a1849111f306
DAEUN9 <gain3436@gmail.com>
2023-08-21 00:26:11 +0900

    TIL : 프로그래머스 python 풀이

--- Soving/programmers/level3/표 편집.py
+++ Soving/programmers/level3/표 편집.py
@@ -1,14 +1,14 @@
 def solution(n, k, cmd):
     answer = ''
     array = [[-1, 1, False]]
-    for i in range(1, n-1):
-        array.append([i-1, i+1, False])
-    array.append([n-2, -1, False])
+    for i in range(1, n - 1):
+        array.append([i - 1, i + 1, False])
+    array.append([n - 2, -1, False])
     curr = k
     deleted = []
     for cm in cmd:
         if len(cm) == 1:
-            if cm =="C":
+            if cm == "C":
                 deleted.append(curr)
                 array[curr][2] = True
                 prev = array[curr][0]
@@ -25,24 +25,11 @@ def solution(n, k, cmd):
                 prev = array[re][0]
                 next = array[re][1]
                 array[re][2] = False
-                while array[re][0] != -1:
-                    if not array[prev][2]:
-                        next = array[prev][1]
-                        array[prev][1] = re
-                        array[next][0] = re
-                        array[re][0] = prev
-                        array[re][1] = next
-                        break
-                    if array[prev][0] == -1:
-                        array[re][0] = -1
-                        while array[re][1] != -1:
-                            if not array[next][2]:
-                                prev = array[next][0]
-                                array[prev][1] = re
-                                array[next][0] = re
-                                array[re][0] = prev
-                                array[re][1] = next
-                                break
+                if prev != -1:
+                    array[prev][1] = re
+                if next != -1:
+                    array[next][0] = re
+
 
         else:
             dr, cnt = cm.split()
@@ -51,10 +38,10 @@ def solution(n, k, cmd):
                 d = 0
             for i in range(int(cnt)):
                 curr = array[curr][d]
+
     for a, b, bo in array:
         if bo:
             answer += 'X'
         else:
             answer += 'O'
     return answer
-solution(	8, 2, ["D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z"])
