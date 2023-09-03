c16fda172fc7cdbaa9f16fece9e68bc65517f69b
DAEUN9 <gain3436@gmail.com>
2023-09-03 22:52:15 +0900

    TIL : 백준 python 풀이

--- ifif/python/S2_3098.py
+++ ifif/python/S2_3098.py
@@ -1,34 +1,36 @@
 import sys
-from collections import deque
 sys.stdin = open("input.txt", "r")
 
 N, M = map(int, input().split())
-relation = [[0 for _ in range(N+1)] for i in range(N+1) ]
+relation = [[0]*(N+1) for _ in range(N+1)]
 visited = [0]*(N+1)
+answer = []
 dq = set()
 for _ in range(M):
     a, b = map(int, input().split())
-    relation[a][b], relation[b][a] = 1, 1
-answer = 0
+    relation[a][b] = 1
+    relation[b][a] = 1
+for i in range(1, N+1):
+    relation[i][i] = 1
 answer_li = []
-while True:
-    answer += 1
-    cnt = 0
-    for k in range(1, N+1):
-        for i in range(1, N+1):
-            for j in range(1, N+1):
-                if i == j:
-                    continue
-                if relation[i][k] and relation[k][j] and not relation[i][j]:
-                    cnt += 1
-                    relation[i][j] = relation[j][i] = answer
-    flag = True
+flag = True
+while flag:
+    flag = False
+    curr = 0
+    new = set()
     for i in range(1, N+1):
         for j in range(1, N+1):
-            if relation[i][j] == 0:
-                flag=False
-                break
-    answer_li.append(cnt)
-    if flag:
-        break
-print(answer_li)
+            for k in range(1, N+1):
+                if relation[i][j] and not relation[i][k]:
+                    new.add((i, k))
+    for a, b in new:
+        if relation[a][b] == 0:
+            relation[a][b], relation[b][a] = 1, 1
+            curr += 1
+    answer.append(curr)
+    for j in range(1, N + 1):
+        for k in range(1, N + 1):
+            if not relation[j][k]:
+                flag = True
+
+print(answer)
--- ifif/python/sol.py
+++ ifif/python/sol.py
@@ -1,29 +1,27 @@
 import sys
 from collections import deque
 sys.stdin = open("input.txt", "r")
-
-input = sys.stdin.readline
-N = int(input())
-dq = deque()
-st = []
-for _ in range(N):
-    req = input().strip("\n")
-    if len(req) == 1:
-        if st and st[-1] == "1":
-            st.pop()
-            dq.pop()
-        elif st:
-            st.pop()
-            dq.popleft()
-    else:
-        comm, alpha = req.split()
-        if comm == "1":
-            dq.append(alpha)
-            st.append(comm)
-        else:
-            dq.appendleft(alpha)
-            st.append(comm)
-if dq:
-    print("".join(dq))
-else:
-    print(0)
+def solution():
+    a, b = input().split()
+    N, M = map(int, input().split())
+    d = dict()
+    visited = dict()
+    for _ in range(M):
+        x, y = input().split()
+        temp1 = d.get(x, [])
+        temp2 = d.get(y, [])
+        d[x], d[y] = temp1 + [y], temp2 + [x]
+    dq = deque([a])
+    visited[a] = 1
+    if a == b:
+        return 0
+    while dq:
+        curr = dq.popleft()
+        for next in d[curr]:
+            if not visited.get(next):
+                visited[next] = visited[curr] + 1
+                if next == b:
+                    return visited[next]-1
+                dq.append(next)
+    return -1
+print(solution())
