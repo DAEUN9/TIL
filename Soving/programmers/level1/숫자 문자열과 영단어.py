64a042d0a25c09b048ce3764a1c4bc2e80208dcb
DAEUN9 <gain3436@gmail.com>
2023-09-10 23:11:25 +0900

    TIL : 프로그래머스 python 풀이

---
+++ Soving/programmers/level1/숫자 문자열과 영단어.py
@@ -0,0 +1,16 @@
+def solution(s):
+    d = {
+        "zero" : 0,
+        "one" : 1,
+        "two" : 2,
+        "three" : 3,
+        "four" : 4,
+        "five" : 5,
+        "six" : 6,
+        "seven" : 7,
+        "eight" : 8,
+        "nine" : 9
+    }
+    for key, val in d.items():
+        s = s.replace(key, str(val))
+    return int(s)
